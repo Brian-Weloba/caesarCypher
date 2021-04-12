95792592bb75137ab721254612aa7cf40865d19a
Brian <bweloba@gmail.com>
2021-04-12 19:08:07 +0300

    add tests for getters and setters

--- src/test/java/CypherTest.java
+++ src/test/java/CypherTest.java
@@ -1,5 +1,20 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CypherTest {
+    @Test
+    public void getmKey_getsKey_2() {
+        Cypher cypher = new Cypher(2,"Hello");
+        int key = cypher.getmKey();
+        assertEquals(2,key);
+    }
+
+    @Test
+    public void getInput_getsInput_Hello() {
+        Cypher cypher = new Cypher(2, "Hello");
+        String input = cypher.getmInput();
+        assertEquals("Hello", input);
+    }
 
 }
