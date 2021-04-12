2512532becf782e5bc5f2fafce61caccf279d5c4
Brian <bweloba@gmail.com>
2021-04-12 19:08:36 +0300

    create method to validate input

--- src/main/java/Cypher.java
+++ src/main/java/Cypher.java
@@ -1,12 +1,39 @@
 public class Cypher {
-    private int mkey;
+    private int mKey;
     private String mInput;
 
-    public int getMkey() {
-        return mkey;
+    public int getmKey() {
+        return mKey;
     }
 
     public String getmInput() {
         return mInput;
+    }
+
+    public Cypher(int mKey, String mInput) {
+        this.mKey = mKey;
+        this.mInput = mInput;
+    }
+
+    public Cypher() {
+    }
+
+    public boolean isInputValid(String mInput) {
+
+        if (mInput.isEmpty() || mInput.equals(" ")) {
+            System.out.println("Input is not valid");
+        }
+
+        char[] inputArray = mInput.toCharArray();
+
+        for (char c : inputArray) {
+            if (!Character.isLetter(c) && c != ' ') {
+                System.out.println("Input is not valid");
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
